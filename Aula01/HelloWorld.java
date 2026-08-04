985372a0bbb4fc6ac8ca246d9681b52afb63ac6f
ryannvini10@gmail.com <unset>
2026-08-03 23:14:13 -0300

    Ex em java

---
+++ Aula01/HelloWorld.java
@@ -0,0 +1,11 @@
+
+
+// Classe chamada HelloWorld 
+public class HelloWorld {
+
+    //Cria uma função 
+    public static void main(String[] args){
+      System.out.println("Hello World"); // imprime informação no console
+    }
+
+}
