be213da211aca00728a0764137ca8feb17a32e53
ryannvini10-beep <ryannvini10@gmail.com>
2026-08-10 21:00:50 -0300

    Aula 02 classe

---
+++ Aula02/Carro.java
@@ -0,0 +1,23 @@
+// Cria a classe chamada carro
+
+package aula02 // package é por conta da pasta
+
+
+class Carro {
+// Atributos 
+// Cria uma variavel  marca do tipo String
+String marca;
+String cor; 
+
+// Metodo (Ação)
+
+void buzinar (){
+    // Exibir mensagem no terminal
+    System.out.println(x: "Bibi");
+}
+
+}
+
+
+
+
